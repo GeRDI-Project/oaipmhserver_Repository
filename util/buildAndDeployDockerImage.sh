8ed98f6387d292950eba150bd68f1acaf8aedcd8
Tobias Weber <tobias.weber@lrz.de>
2017-12-07 17:58:19 +0100

    [BUGFIX] wrong image tag fixed

--- util/buildAndDeployDockerImage.sh
+++ util/buildAndDeployDockerImage.sh
@@ -4,15 +4,15 @@ dockerRegistry="docker-registry.gerdi.research.lrz.de:5043"
 imageName="archive/oaipmhserver"
 imageUrl="${dockerRegistry}/${imageName}"
 
-docker build -t "${imageName}:latest" .
-docker push "${dockerRegistry}/${imageName}:latest"
+docker build -t "${imageUrl}:latest" .
+docker push "${imageUrl}:latest"
 
 gittags=$(git tag -l --points-at HEAD)
 if [ ! -z "$gittags" ]
 then
     for gittag in $gittags
     do
-        docker tag "${imageName}" "${imageName}:${gittag}"
-        docker push "${dockerRegistry}/${imageName}:${gittag}"
+        docker tag "${imageUrl}" "${imageUrl}:${gittag}"
+        docker push "${imageUrl}:${gittag}"
     done
 fi
