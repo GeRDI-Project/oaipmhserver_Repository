b4021238181f980e26451c46b37edc435c4024d4
Tobias Weber <tobias.weber@lrz.de>
2017-12-07 17:50:23 +0100

    Script now builds and pushes images

--- util/buildAndDeployDockerImage.sh
+++ util/buildAndDeployDockerImage.sh
@@ -4,13 +4,15 @@ dockerRegistry="docker-registry.gerdi.research.lrz.de:5043"
 imageName="archive/oaipmhserver"
 imageUrl="${dockerRegistry}/${imageName}"
 
-docker build -t "${imageName}:latest" ..
+docker build -t "${imageName}:latest" .
+docker push "${dockerRegistry}/${imageName}:latest"
 
 gittags=$(git tag -l --points-at HEAD)
 if [ ! -z "$gittags" ]
 then
     for gittag in $gittags
     do
-        docker tag "${imageName}:${gittag}"
+        docker tag "${imageName}" "${imageName}:${gittag}"
+        docker push "${dockerRegistry}/${imageName}:${gittag}"
     done
 fi
