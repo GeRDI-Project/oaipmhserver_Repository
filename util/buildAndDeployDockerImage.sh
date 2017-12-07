29e65674c602ad4acad67379ac7d39d09a1d8232
Tobias Weber <tobias.weber@lrz.de>
2017-12-07 18:24:47 +0100

    Build only on master

--- util/buildAndDeployDockerImage.sh
+++ util/buildAndDeployDockerImage.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
-
-dockerRegistry="docker-registry.gerdi.research.lrz.de:5043"
-imageName="archive/oaipmhserver"
-imageUrl="${dockerRegistry}/${imageName}"
-
-docker build -t "${imageUrl}:latest" .
-docker push "${imageUrl}:latest"
-
-gittags=$(git tag -l --points-at HEAD)
-if [ ! -z "$gittags" ]
+branch=$(git rev-parse --abbrev-ref HEAD)
+if [ "$branch" = "master" ]
 then
-    for gittag in $gittags
-    do
-        docker tag "${imageUrl}" "${imageUrl}:${gittag}"
-        docker push "${imageUrl}:${gittag}"
-    done
+    dockerRegistry="docker-registry.gerdi.research.lrz.de:5043"
+    imageName="archive/oaipmhserver"
+    imageUrl="${dockerRegistry}/${imageName}"
+
+    docker build -t "${imageUrl}:latest" .
+    docker push "${imageUrl}:latest"
+
+    gittags=$(git tag -l --points-at HEAD)
+    if [ ! -z "$gittags" ]
+    then
+        for gittag in $gittags
+        do
+            docker tag "${imageUrl}" "${imageUrl}:${gittag}"
+            docker push "${imageUrl}:${gittag}"
+        done
+    fi
+else
+    echo "On branch $branch - will not build (open a PR to master to build and deploy)"
 fi
