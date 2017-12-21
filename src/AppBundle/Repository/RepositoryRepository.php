c5e2b4a27a6ea03002dadb33e73c1c5664ba7aec
Tobias Weber <tobias.weber@lrz.de>
2017-12-21 14:45:54 +0100

    added license information to automatically created repository files

--- src/AppBundle/Repository/RepositoryRepository.php
+++ src/AppBundle/Repository/RepositoryRepository.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * This file is part of the GeRDI software suite
+ *
+ * @author  Tobias Weber <weber@lrz.de>
+ * @license https://www.apache.org/licenses/LICENSE-2.0
+ */
 namespace AppBundle\Repository;
 
 /**
