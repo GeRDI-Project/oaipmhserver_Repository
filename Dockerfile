c727dd06cb3dc9bd226dea30c8d2b32124f1c497
Tobias Weber <tobias.weber@lrz.de>
2018-01-16 18:31:27 +0100

    [SAI-458] Dockerfile created (and pushed)

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,22 @@
-FROM fauria/lamp 
+FROM php:7.2.1-apache-stretch
+RUN apt-get update && apt-get install -y \
+    sqlite3 \
+    libsqlite3-dev
 
-ADD . /app
+RUN docker-php-source extract
+RUN docker-php-ext-install -j$(nproc) pdo_sqlite
+RUN docker-php-source delete
 
-RUN /app/util/prepareImage.sh
+COPY . /var/www/
 
-ADD app/config/config.yml.deploy /app/app/config/config.yml
+RUN sed -i 's#/usr/sbin/nologin#/bin/bash#' /etc/passwd
+#RUN sed -i '#DocumentRoot /var/www/html#a SetEnv APP_ENV=prod' /etc/apache2/sites-enabled/000-default.conf
+ADD app/config/config.yml.deploy /var/www/app/config/config.yml
 
-RUN /app/bin/console cache:clear
-RUN /app/bin/console cache:warmup
+RUN chown -R www-data:www-data /var/www
 
-RUN chown -R www-data:www-data /app
+RUN su -c /var/www/util/prepareImage.sh www-data
+RUN su -c '/var/www/bin/console cache:clear --env=prod' www-data
+RUN su -c '/var/www/bin/console cache:warmup --env=prod' www-data
+
+
--- util/prepareImage.sh
+++ util/prepareImage.sh
@@ -3,13 +3,15 @@
 # This file is part of the GeRDI software suite
 # Author Tobias Weber <weber@lrz.de> 
 # License: https://www.apache.org/licenses/LICENSE-2.0
-# Absolutely no warranty given!
+# Absolutely no warranty given! 
 
-apt-get remove php7.0-snmp -y
-apt-get install zip -y
-sed -i 's#/var/www/html#/app/web#g' /etc/apache2/sites-available/000-default.conf
-sed -i 's#/var/www#/app/web#g' /etc/apache2/apache2.conf
-ln -s /app/web /var/www/html
-
-cd /app
-composer install
+#sed -i 's#/var/www/html#/app/web#g' /etc/apache2/sites-available/000-default.conf
+#sed -i 's#/var/www#/app/web#g' /etc/apache2/apache2.conf
+rm -r /var/www/html
+ln -s /var/www/web/ /var/www/html
+cd /var/www
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+php composer.phar install
