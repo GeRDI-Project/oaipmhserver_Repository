21a248ba975fa906a074d15ef7c2be2e49bbf22f
Rinkita <Rinkita.Mittal@lrz.de>
2018-08-27 09:28:12 +0200

    Included symphony/assetic-bundle in composer.json to be installed as prerequisite

--- util/prepareImage.sh
+++ util/prepareImage.sh
@@ -22,10 +22,14 @@ php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
-php composer.phar require symfony/assetic-bundle
 php composer.phar install
-echo '-----installed composer-----'
-php composer.phar update
-echo '-----Updated composer-----'
-bin/console assets:install --symlink
+#php composer.phar require symfony/assetic-bundle
+
+
+#echo '-----installed composer-----'
+#php composer.phar update
+#echo '-----Updated composer-----'
+
+#su -c '/var/www/bin/console cache:clear --env=prod' www-data
+#su -c '/var/www/bin/console cache:warmup --env=prod' www-data
 
